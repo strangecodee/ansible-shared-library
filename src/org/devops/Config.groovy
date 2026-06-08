ed9e51c638783c25c2a72799f66893620d0404e0
Anurag <131364622+strangecodee@users.noreply.github.com>
2026-06-08 18:01:29 +0530

    Add Config class for loading configurations

---
+++ src/org/devops/Config.groovy
@@ -0,0 +1,11 @@
+package org.devops
+
+class Config {
+
+    static Map load(script, String fileName) {
+
+        script.echo "========== Loading Configuration =========="
+
+        return script.readProperties file: fileName
+    }
+}
